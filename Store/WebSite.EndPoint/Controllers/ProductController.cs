9f39f35e965c91aa44fffd102f55c2f60ba5dc58
Ehsan Seyedzadeh <e.seyedzadeh73@Gmail.com>
2022-10-14 13:26:49 +0330

    PLP Front-End

---
+++ Store/WebSite.EndPoint/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using Application.Catalogs.CatalogItems.GetCatalogIItemPLP;
+using Application.Catalogs.CatalogItems.GetCatalogItemPDP;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebSite.EndPoint.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IGetCatalogIItemPLPService getCatalogIItemPLPService;
+
+        public ProductController(IGetCatalogIItemPLPService getCatalogIItemPLPService)
+        {
+            this.getCatalogIItemPLPService = getCatalogIItemPLPService;
+        }
+        public IActionResult Index(int page = 1, int pageSize = 20)
+        {
+            var data = getCatalogIItemPLPService.Execute(page, pageSize);
+            return View(data);
+        }
+
+    }
+}
+
