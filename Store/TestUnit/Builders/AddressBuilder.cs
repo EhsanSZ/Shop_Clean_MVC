24084a56a5558669ad82026144392b4d8e1a50cb
Ehsan Seyedzadeh <e.seyedzadeh73@Gmail.com>
2022-10-27 10:09:05 +0330

    Test

---
+++ Store/TestUnit/Builders/AddressBuilder.cs
@@ -0,0 +1,34 @@
+﻿using Domain.Order;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest.Builders
+{
+    public class AddressBuilder
+    {
+        private Address _address;
+        public string TestCity => "Tehran";
+        public string TestState => "Jeyhon";
+        public string TestZipCode => "2342345";
+        public string TestPostalAddress => "Ehsan Tower";
+
+
+        public AddressBuilder()
+        {
+            _address = WithDefaultValues();
+        }
+
+        public Address Build()
+        {
+            return _address;
+        }
+        private Address WithDefaultValues()
+        {
+            _address = new Address(TestCity, TestState, TestZipCode, TestPostalAddress);
+            return _address;
+        }
+    }
+}
